5e0a794b16101fb52c3a07a849e7c598dcedca4c
Angus.Fenying <i.am.x.fenying@gmail.com>
2019-06-28 19:38:41 +0800

    Check the debug and package.json.

--- src/examples/debug.ts
+++ src/examples/debug.ts
@@ -8,56 +8,48 @@ import * as L from "@litert/core";
     const cli = Redis.createCommandClient("127.0.0.1", 6373);
     const sub = Redis.createSubscriberClient("127.0.0.1", 6373);
 
-    try {
+    await sub.connect();
 
-        await sub.connect();
+    await sub.auth("hello");
 
-        await sub.auth("hello");
+    sub.on("message", function(c, d, p): void {
 
-        sub.on("message", function(c, d, p): void {
+        console.log(`Channel: ${c}, Data: ${d.toString()}`);
+    });
 
-            console.log(`Channel: ${c}, Data: ${d.toString()}`);
-        });
+    await sub.subscribe(["hello"]);
 
-        await sub.subscribe(["hello"]);
+    await cli.connect();
+    await cli.auth("hello");
+    console.log(await cli.ping(""));
+    console.log(await cli.set("a", "123"));
+    console.log(await cli.incr("a", 23));
+    console.log(await cli.incrByFloat("a", 23.4));
+    console.log(await cli.get("a"));
+    console.log(await cli.mGet(["a", "sada"]));
 
-        await cli.connect();
-        await cli.auth("hello");
-        console.log(await cli.ping(""));
-        console.log(await cli.set("a", "123"));
-        console.log(await cli.incr("a", 23.4));
-        console.log(await cli.get("a"));
-        console.log(await cli.mGet(["a", "sada"]));
+    await L.Async.sleep(2000);
 
-        await L.Async.sleep(2000);
+    let x = cli.set("a", "333");
 
-        let x = cli.set("a", "333");
+    console.log(await x);
+    await cli.lPush("lll", ["a", "b"]);
+    console.log(await cli.lIndex("lll", 1));
+    console.log(await cli.lRange("lll", 0, -1));
+    console.log(await cli.hMSet("ggg", {
+        "a": "3333",
+        "bb": "1231232131"
+    }));
+    console.log(await cli.hMGet("ggg", ["bb", "a"]));
+    console.log(await cli.incr("a"));
+    console.log(await cli.pubSubChannels());
+    console.log(await cli.pubSubNumPat());
+    console.log(await cli.pubSubNumSub(["hello"]));
+    console.log(await cli.publish("hello", "test"));
 
-        console.log(await x);
-        await cli.lPush("lll", ["a", "b"]);
-        console.log(await cli.lIndex("lll", 1));
-        console.log(await cli.lRange("lll", 0, -1));
-        console.log(await cli.hMSet("ggg", {
-            "a": "3333",
-            "bb": "1231232131"
-        }));
-        console.log(await cli.hMGet("ggg", ["bb", "a"]));
-        console.log(await cli.incr("a"));
-        console.log(await cli.pubSubChannels());
-        console.log(await cli.pubSubNumPat());
-        console.log(await cli.pubSubNumSub(["hello"]));
-        console.log(await cli.publish("hello", "test"));
-
-        await L.Async.sleep(2000);
-
-        await cli.shutdown();
-        await sub.shutdown();
-    }
-    catch (e) {
-
-        console.error(e);
-    }
+    await L.Async.sleep(2000);
 
     await cli.shutdown();
+    await sub.shutdown();
 
 })();
